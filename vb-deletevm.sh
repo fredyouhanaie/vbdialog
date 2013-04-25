4ebda2e1c833289e23465dfa00fdc32f6f3a73bc
Fred Youhanaie <fy@localhost>
2013-04-25 06:28:05 +0100

    now we can delete a VM

---
+++ vb-deletevm.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# vb-deletevm
+#	Delete VM
+
+pdir=`dirname $0`
+pname=`basename $0`
+
+[ -n "$VBFUNCTIONS" ] || source $pdir/vbfunctions.sh
+
+#
+#	set the background title, unless already set by caller
+#
+: ${backtitle:="Virtual Box"}
+backtitle="$backtitle - Delete VM"
+
+VBDELETE='VBoxManage unregistervm --delete'
+
+while :
+do
+	VMLIST=`VBoxManage list vms | sed 's/["{}]//g'`
+	vm=`dialog --stdout --backtitle "$backtitle" --title "List of current VMs" \
+		--menu 'Select a VM to delete, or <Cancel> to return' 0 0 0 $VMLIST `
+	[ $? = 0 ] || break
+	runcommand "Ready to Delete VM?" "$VBDELETE $vm"
+	[ $? = 0 ] && break
+done
+
+clear
+echo "$pname: Terminated!" >&2
